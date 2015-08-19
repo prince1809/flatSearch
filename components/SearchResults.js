03795b4b95053c460cb32a504afcdc01b5844725
Prince Kumar <prince18pat@gmail.com>
2015-08-19 17:48:45 +0900

    Fetch data

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -64,8 +64,75 @@ var SearchResults = React.createClass({
   queryRMLS: function(){
     var search = this.props.search;
 
-    console.log(search);
+    var options = [
+			'ReportID=RC_RESULT',
+			'SearchType=RC',
+			'FormID=RC_SEARCH_RESIDENTIAL',
+			'FormURL=%2Frc2%2FUI%2Fsearch_residential.asp',
+			'PropType=1',
+			'STATE_ID=..%2F..%2FRC2%2FUI%2Fsearch_residential.asp',
+			'PROPERTY_TYPE='+encodeURIComponent(search.propertyType || ''),
+			'PRICE='+encodeURIComponent(search.priceRange[0] || ''),
+			'PRICE='+encodeURIComponent(search.priceRange[1] || ''),
+			'INCLUDE_AUCTION=3',
+			'STATUS=ACT',
+			'STATUS=BMP',
+			'YEAR_BUILT='+encodeURIComponent(search.builtRange[0] || ''),
+			'YEAR_BUILT='+encodeURIComponent(search.builtRange[1] || ''),
+			'BEDROOMS='+encodeURIComponent(search.bedrooms[0] || ''),
+			'BEDROOMS='+encodeURIComponent(search.bedrooms[1] || ''),
+			'BATHROOMS='+encodeURIComponent(search.bathrooms[0] || ''),
+			'BATHROOMS='+encodeURIComponent(search.bathrooms[1] || ''),
+			'ZIP='+encodeURIComponent(search.zipCodes[0] || ''),
+			'ZIP='+encodeURIComponent(search.zipCodes[1] || ''),
+			'ZIP='+encodeURIComponent(search.zipCodes[2] || ''),
+			'ZIP='+encodeURIComponent(search.zipCodes[3] || ''),
+			'SQFT=',
+			'SQFT=',
+			'ACRES=',
+			'ACRES=',
+			'SCHOOL_TYPE=Elementary',
+			'SCHOOL_NAME=',
+			'ACCESSIBILITY_YN=',
+			'OHBT_DATE=',
+			'COUNTY=',
+			'CITY=172',
+			'BANK_OWNED_YN=',
+			'SHORT_SALE_YN=',
+		];
+
+    this.setState({
+      searchPending: true
+    });
+
+    var serialized = options.join('&');
+
+		fetch('http://www.rmls.com/rc2/engine/sqlGenerator_RmlsCom.asp', {
+			method: 'post',
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36",
+				"Referer": "http://www.rmls.com/rc2/UI/search_residential.asp"
+			},
+			body: serialized
+		})
+		.then((response) => response.text())
+		.then((responseText) => {
+      //console.log(responseText);
+			this.processsResults(responseText);
+
+		})
+		.catch(function (error) {
+			console.error('An error occured');
+			console.error(error.message);
+		});
   },
+
+  processsResults: function(html){
+    console.log(html);
+
+  },
+
   render: function(){
     return(
       <View style={styles.container}>
